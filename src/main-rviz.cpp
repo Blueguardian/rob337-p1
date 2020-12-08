8a29f86cf0049faabc742a2c88d84bd5d7708185
Daniel Doroshenko <daniel.d@outlook.dk>
2020-12-08 11:45:00 +0100

    Fix in exhib_function exection

--- src/main-rviz.cpp
+++ src/main-rviz.cpp
@@ -21,13 +21,13 @@ double anglez = 0;
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 void userInterface_cb(const geometry_msgs::PoseStamped::ConstPtr &msg);                                                      //Prints messeges containing the received coordinates                                                             //Prints messeges containing the received coordinates
 void goal_reached_cb(const actionlib::SimpleClientGoalState &state, const move_base_msgs::MoveBaseResult::ConstPtr &result); //Goal has been reached                                                             //Odometry callback function
-void send_goal(move_base_msgs::MoveBaseGoal goal_point, int i);                                                                //Send goal to move_base server
-void send_marker(move_base_msgs::MoveBaseGoal goal); 
+void send_goal(move_base_msgs::MoveBaseGoal goal_point, int i);                                                              //Send goal to move_base server
+void send_marker(move_base_msgs::MoveBaseGoal goal);
 double rob_facing_angle(double angle);
 move_base_msgs::MoveBaseGoal get_dif2Dgoal(move_base_msgs::MoveBaseGoal goal);
 void sortCoord(std::vector<move_base_msgs::MoveBaseGoal> target, int startpos, int itera, double refx, double refy);
 double euclidianDist(double x1, double y1, double refx, double refy);
-void exhib_scan(move_base_msgs::MoveBaseGoal goal);                                                                
+void exhib_scan(move_base_msgs::MoveBaseGoal goal);
 
 int main(int argc, char **argv)
 {
@@ -48,27 +48,27 @@ int main(int argc, char **argv)
   {
     int i = 0;
     int start = getchar();
-    while(start == 't' && i < targets.size())
+    while (start == 't' && i < targets.size())
     {
       int end = getchar();
-      if(i == 0)
+      if (i == 0)
       {
         sortCoord(targets, i, targets.size(), 0, 0);
         send_goal(targets[i], i);
       }
       else
       {
-        sortCoord(targets, i, targets.size(), targets[i-1].target_pose.pose.position.x, targets[i-1].target_pose.pose.position.y);
+        sortCoord(targets, i, targets.size(), targets[i - 1].target_pose.pose.position.x, targets[i - 1].target_pose.pose.position.y);
         send_goal(targets[i], i);
       }
-      if(end == 'q')
+      if (end == 'q')
       {
         ros::shutdown();
       }
       i++;
     }
     ros::spinOnce();
-    if(start == 'q')
+    if (start == 'q')
     {
       ros::shutdown();
     }
@@ -138,10 +138,10 @@ void send_goal(move_base_msgs::MoveBaseGoal goal_point, int i)
   MoveBaseClient ac("move_base", true);
   ac.sendGoal(goal_point, boost::bind(&_goal_reached_cb, _1, _2));
   ROS_INFO("Sending goal and markers..");
-//  send_marker(goal_point);
+  //  send_marker(goal_point);
   ros::Publisher marker_pub = ptrnh->advertise<visualization_msgs::MarkerArray>("visualization_marker", 1);
   marker_pub.publish(markers);
-  markers.markers.erase(markers.markers.begin()+i);
+  markers.markers.erase(markers.markers.begin() + i);
   ac.waitForResult();
   exhib_scan(goal_point);
 }
@@ -168,8 +168,8 @@ double rob_facing_angle(double angle)
 
 void send_marker(move_base_msgs::MoveBaseGoal goal)
 {
-//  ros::Publisher marker_pub;
-//  marker_pub = ptrnh->advertise<visualization_msgs::MarkerArray>("exhibit_markers", 1);
+  //  ros::Publisher marker_pub;
+  //  marker_pub = ptrnh->advertise<visualization_msgs::MarkerArray>("exhibit_markers", 1);
   visualization_msgs::Marker marker;
   marker.header.stamp = ros::Time::now();
   marker.ns = "target_point";
@@ -193,7 +193,6 @@ void send_marker(move_base_msgs::MoveBaseGoal goal)
   marker.pose.position.z += marker.scale.x;
   markers.markers.push_back(marker);
   ROS_INFO("Storing marker for publishing"); //For testing purposes.
-
 }
 
 move_base_msgs::MoveBaseGoal get_dif2Dgoal(move_base_msgs::MoveBaseGoal goal)
@@ -222,10 +221,11 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
   double perp_line_angle = 0; //Calculation of perpendicular angle, used in increment
   double increment_x = 0;
   double increment_y = 0;
+  bool skip = false;
   MoveBaseClient ac1("move_base", true);
 
   if (((fabs(anglez) > 0) && (fabs(anglez) < M_PI_2))) //Angle in fist quadrant
-  {                                                                                            //This means the angle can be calculated as
+  {                                                    //This means the angle can be calculated as
     perp_line_angle = fabs(atan(-1 / (tan(fabs(anglez)))));
   }
   else if (((fabs(anglez) > M_PI_2) && (fabs(anglez) < M_PI))) //Second quadrant
@@ -238,7 +238,7 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
   }
   else if ((((fabs(anglez) > (2 * M_PI * (3 / 4)))) && (fabs(anglez) < (2 * M_PI)))) //Angle in fourth quadrant
   {
-    perp_line_angle = fabs(atan(-1 / (tan(fabs(anglez- (2 * M_PI))))));
+    perp_line_angle = fabs(atan(-1 / (tan(fabs(anglez - (2 * M_PI))))));
   }
 
   //We now do calculations which we assign to the datatype goal (x,y and z) in order for the robot to move right/left at the exhibit and take an image.
@@ -262,10 +262,11 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
       goal.target_pose.pose.position.y = goal.target_pose.pose.position.y - increment_y;
       ac1.sendGoalAndWait(goal);
     }
+    skip = true;
   }
 
   else if (((fabs(anglez) < (M_PI_2 + 0.01)) && (fabs(anglez) > (M_PI_2 - 0.01))) || ((fabs(anglez) < (((3 / 4) * 2 * M_PI) + 0.01)) && (fabs(anglez) > (((3 / 4) * 2 * M_PI) - 0.01)))) //If cos(angle) = apprx. 1 //6.28 = ca. 2*Pi
-  {                     //The robot has its direction 90 or 270 degrees
+  {                                                                                                                                                                                      //The robot has its direction 90 or 270 degrees
     increment_x = 0;
     increment_y = step;
     for (int i = 0; i < 3; i++)
@@ -282,6 +283,7 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
       goal.target_pose.pose.position.y = goal.target_pose.pose.position.y - increment_y;
       ac1.sendGoalAndWait(goal);
     }
+    skip = true;
   }
   else //The robot is facing somewhere between - calculations for steps required!
   {
@@ -289,7 +291,7 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
     increment_y = step * sin(perp_line_angle);
   }
 
-  if (((perp_line_angle > 0) && (perp_line_angle < M_PI_2)) || ((perp_line_angle > M_PI) && (perp_line_angle < (2 * M_PI * (3 / 4))))) //Robot facing either first or third quadrant
+  if (((perp_line_angle > 0) && (perp_line_angle < M_PI_2) && (skip == false)) || (((perp_line_angle > M_PI) && (perp_line_angle < (2 * M_PI * (3 / 4)))) && (skip == false))) //Robot facing either first or third quadrant
   {
     for (int i = 0; i < 3; i++)
     { //We make the robot move 3 steps to the right, in which it faces the exhibits
@@ -306,7 +308,7 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
       ac1.sendGoalAndWait(goal);
     }
   }
-  else
+  else if (skip == false)
   {
     for (int i = 0; i < 3; i++)
     {
@@ -330,31 +332,31 @@ void exhib_scan(move_base_msgs::MoveBaseGoal goal)
 
 void sortCoord(std::vector<move_base_msgs::MoveBaseGoal> target, int startpos, int itera, double refx, double refy)
 {
-    //The function takes an array, a starting position, a number of iterations, since it ensures that the array does not get too big,
-    //and takes a set of coordinates for the point of reference, It then compares the array's coordinatesets by calling the euclidianDist() function
-    //to compare them by their euclidian distance. It then switches the sets if the former set is smaller than the latter.
+  //The function takes an array, a starting position, a number of iterations, since it ensures that the array does not get too big,
+  //and takes a set of coordinates for the point of reference, It then compares the array's coordinatesets by calling the euclidianDist() function
+  //to compare them by their euclidian distance. It then switches the sets if the former set is smaller than the latter.
 
-    //beginning of function
+  //beginning of function
 
-    for(int i = startpos; i<itera; i++) //iterator for the first coordinateset
+  for (int i = startpos; i < itera; i++) //iterator for the first coordinateset
+  {
+    if ((euclidianDist(target[startpos].target_pose.pose.position.x, target[startpos].target_pose.pose.position.y, refx, refy) > (euclidianDist(target[i].target_pose.pose.position.x, target[i].target_pose.pose.position.y, refx, refy))))
     {
-        if((euclidianDist(target[startpos].target_pose.pose.position.x, target[startpos].target_pose.pose.position.y, refx, refy) > (euclidianDist(target[i].target_pose.pose.position.x, target[i].target_pose.pose.position.y, refx, refy))))
-        {
-                //switches the places of the coordinateset if it's smaller.
-                std::swap(target[startpos], target[i]);
-        }
+      //switches the places of the coordinateset if it's smaller.
+      std::swap(target[startpos], target[i]);
     }
-} 
+  }
+}
 
 double euclidianDist(double x1, double y1, double refx, double refy)
 {
-    //This function takes in two coordinates of type double (x1 and y1) and calculates the distance from a point of
-    //reference (refx and refy) and returns the euclidian distance between these.
+  //This function takes in two coordinates of type double (x1 and y1) and calculates the distance from a point of
+  //reference (refx and refy) and returns the euclidian distance between these.
 
-    //beginning of function
+  //beginning of function
 
-    double distx = pow(x1-refx, 2); //Reference distance calculation
-    double disty = pow(y1-refy, 2); //Input distance calculation
-    double dist = sqrt(distx+disty); //calculation of distance between reference point and input point
-    return dist;
+  double distx = pow(x1 - refx, 2);  //Reference distance calculation
+  double disty = pow(y1 - refy, 2);  //Input distance calculation
+  double dist = sqrt(distx + disty); //calculation of distance between reference point and input point
+  return dist;
 }
